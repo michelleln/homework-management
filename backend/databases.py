38ba2289d971d2eb80b1d14898f3c96742d0b3be
Hong Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-11-30 03:29:39 -0500

    seet up helpers

--- backend/databases.py
+++ backend/databases.py
@@ -1,14 +1,18 @@
 import pymongo
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
 
 class DB(object):
 
-    URI = "mongodb+srv://lehnhung28:ZinGfix3mV8Liw6x@cluster0.k06gnyf.mongodb.net/?retryWrites=true&w=majority"
+    uri = "mongodb+srv://lehnhung28:ZinGfix3mV8Liw6x@cluster0.k06gnyf.mongodb.net/?retryWrites=true&w=majority"
 
     @staticmethod
     def init():
-        client = MongoClient(uri,
-                     tls=True,
-                     tlsCertificateKeyFile='<path_to_certificate>',
-                     server_api=ServerApi('1'))
-
+        client = MongoClient(DB.uri)
+        DB.DATABASE = client['collection']
+        try:
+            client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
